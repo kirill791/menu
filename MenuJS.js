41da94cf4f11e468ca834ae09e8210a4ad23cee0
kirill791 <76863554+kirill791@users.noreply.github.com>
2021-01-02 11:15:45 +0300

    Add files via upload

---
+++ MenuJS.js
@@ -0,0 +1,19 @@
+function clockAssign(){
+    document.location.assign("file:///C:/Users/%D0%98%D0%B2%D0%B0%D0%BD/Downloads/Project/clock/clockHTML.html")
+}
+
+function calculatorAssign(){
+    document.location.assign("file:///C:/Users/%D0%98%D0%B2%D0%B0%D0%BD/Downloads/Project/calculator/calculatorHTML.html")
+}
+
+function snakeAssign(){
+    document.location.assign("file:///C:/Users/%D0%98%D0%B2%D0%B0%D0%BD/Downloads/Project/Snake/SnakeHTML.html")
+}
+
+function sliderAssign(){
+    document.location.assign("file:///C:/Users/%D0%98%D0%B2%D0%B0%D0%BD/Downloads/Project/Slider/SliderHTML.html")
+}
+
+function clickerAssign(){
+    document.location.assign("file:///C:/Users/%D0%98%D0%B2%D0%B0%D0%BD/Downloads/Project/Clicker/ClickerHTML.html")
+}
